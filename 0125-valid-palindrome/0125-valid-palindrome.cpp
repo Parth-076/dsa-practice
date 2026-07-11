067b4fbd314c0c459df0bd86261bdf6ebd54fb9e
Parth-076 <jadhavparth076@gmail.com>
2026-07-11 09:08:37 +0530

    Time: 2 ms (36.27%), Space: 9.9 MB (55.13%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for(int i = 0, j = s.size() - 1; i < j ; ) {
+            if (!isalnum(s[i])) i++;
+            else if (!isalnum(s[j])) j--;
+            else {
+                if(tolower(s[i]) != tolower(s[j])) return false;
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+};
